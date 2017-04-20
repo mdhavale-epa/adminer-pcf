e2aa0608318cd880d47d601311b765ea0723f8b5
mdhavale-epa <dhavale.michael@epa.gov>
2017-04-20 11:08:03 -0400

    initial commit

---
+++ adminer.sql
@@ -0,0 +1,3 @@
+-- Your mysql dump needs to be named "adminer.sql". 
+-- Note: LOCK TABLE and UNLOCK TABLE commands are not supported.  You'll have to strip those from your dump.
+
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cf push -b default adminer-pcf
+
+echo
+echo NOTE: application will be available at:  http://[YOUR_URL]/adminer-4.3.0-en.php
+echo
